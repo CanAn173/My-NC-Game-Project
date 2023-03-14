90b3f94228d017e1e8904f444807f66c99ef576d
canan173 <cacan2003@gmail.com>
2023-03-14 14:18:26 +0000

    move line 19 in app.js

--- db/creatingApp/app.js
+++ db/creatingApp/app.js
@@ -13,11 +13,11 @@ customErrorStatus} = require('./errorHandling');
 
 const cors = require('cors');
 
+
 const app = express();
 
-app.use(express.json());
-
 app.use(cors());
+app.use(express.json());
 
 // REQUESTS:
 
